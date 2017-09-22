14cb8bd29148db02c76920814518f4a5f680fe3e
raghavendra <raghavendra@cloudmpower.com>
2017-09-22 14:43:02 +0530

    aa

---
+++ finalk/app_config.js
@@ -0,0 +1,4 @@
+const appUrl = "https://www.sectrackdemo.in/";
+const verifiedData = ["1","1","0","1","0",];
+const notVerifiedData = ["0","0","-1","0","-1",];
+const labelDisplay = ["10:00","10:05","10:10","10:15","10:20",];
