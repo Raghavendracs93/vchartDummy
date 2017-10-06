cbad64a4f2358dcf0ef506e12dc3d76c1fa36e8c
raghavendra <raghavendra@cloudmpower.com>
2017-10-06 18:06:11 +0530

    update

--- public/owner/script/app_config.js
+++ public/owner/script/app_config.js
@@ -1,5 +1,5 @@
-const appUrl = "https://www.sectrackdemo.in/";
-const webApp =true;
+const appUrl = "https://www.sectrackdev.in/";
+const webApp =false;
 const verifiedData = ["1","1","0","1","0",];
 const notVerifiedData = ["0","0","-1","0","-1",];
 const labelDisplay = ["10:00","10:05","10:10","10:15","10:20",];
